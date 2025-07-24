566d00d4efdfcc10cb3fce69f78edb35082d1821
Neginadg <abdolbaghinegin@gmail.com>
2025-07-24 16:59:53 +0300

    dice and currest score are working

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+  //Generating a random roll dice
+  let dice = Math.trunc(Math.random() * 6) + 1;
+
+  //Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //Check for rolled 1; if true,
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
+});
