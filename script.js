2c7e8183a3c30e12b550fa63293d2afa468f56a3
Negiadg <abdolbaghinegin@gmail.com>
2025-08-15 17:43:14 +0300

    Hold button is working

--- script.js
+++ script.js
@@ -17,9 +17,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const score = [0, 0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -38,10 +46,19 @@ btnRoll.addEventListener('click', () => {
       currentScore;
   } else {
     // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+btnHold.addEventListener('click', () => {
+  //Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //Check if player's score is >= 100
+
+  //Finish the game
+
+  //switch to next player
+  switchPlayer();
 });
